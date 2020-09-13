00f26e8a358188bf6e5edbc42005b997ac5f36fc
Kent Schaeffer <ne0crank@icloud.com>
2020-09-13 11:17:06 -0600

    added npm update to update packages

--- bash/mpull.sh
+++ bash/mpull.sh
@@ -1,3 +1,14 @@
 #!/bin/zsh
 main="/Users/ne0crank/git"
-for h in `ls $main`; do cd ${main}/${h}; echo; echo git pull $h; echo; git pull; cd $main; done
+for h in `ls $main`; do
+  cd ${main}/${h}
+  echo
+  echo git pull $h
+  echo
+  git pull
+  echo
+  echo npm update $h
+  echo
+  npm update
+  cd $main
+done
