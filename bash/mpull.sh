88108efe2d9e72da4f223dde2110c5cf00b99134
Kent Schaeffer <ne0crank@icloud.com>
2021-01-19 15:14:09 -0700

    added condition to mpull to check for directory

--- bash/mpull.sh
+++ bash/mpull.sh
@@ -2,14 +2,21 @@
 main="/Users/ne0crank/git"
 npm i -g npm
 for h in `ls $main`; do
-  cd ${main}/${h}
-  echo
-  echo git pull $h
-  echo
-  git pull
-  echo
-  echo npm update $h
-  echo
-  npm update
-  cd $main
+  if [ -d ${main}/${h} ]; then
+    cd ${main}/${h}
+    echo
+    echo npm update $h
+    echo
+    npm update
+    echo
+    echo git config pull.rebase false
+    echo
+    git config pull.rebase false
+    echo
+    echo git pull $h
+    echo
+    git pull
+    echo
+    cd $main
+  fi
 done
